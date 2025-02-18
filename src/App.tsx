2c61b397ca0605eca6f95e78e6862aa1489db7b5
Alistair Lambert <lambert.alistair96@gmail.com>
2025-02-18 14:12:36 +0000

    Team name and center button

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import {
   SignedOut,
   UserButton,
 } from '@clerk/clerk-react';
+import './index.css';
 
 function App() {
   return (
-    <header>
+    <header style={{ textAlign: 'center' }}>
       <SignedOut>
-        <SignInButton />
+        <div style={{ fontSize: '4em' }}>Team hACkS!</div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <SignInButton />
+        </div>
       </SignedOut>
       <SignedIn>
         <UserButton />
